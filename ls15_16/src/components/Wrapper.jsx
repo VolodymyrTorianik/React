7e0f68b4a0153174e9c1b7c15aebd7051c1aa47e
VolodymyrTorianik <zvittoryanik@gmail.com>
2025-03-17 19:00:40 +0100

    Change Wrapper

--- ls15_16/src/components/Wrapper.jsx
+++ ls15_16/src/components/Wrapper.jsx
@@ -5,18 +5,54 @@ import { Content2 } from "../components/Content2";
 import { Footer } from "../components/Footer";
 
 export function Wrapper() {
-    return (
-        <div>
-            <Navbar />
-            <hr />
+  const [showContent1, setShowContent1] = useState(true);
+  const [showContent2, setShowContent2] = useState(true);
+  const [showFooter, setShowFooter] = useState(true);
+
+  return (
+    <div>
+      <Navbar />
+      <hr />
+      
+      <div>
+        <button onClick={() => setShowContent1(!showContent1)}>
+          {showContent1 ? "Сховати Content1" : "Показати Content1"}
+        </button>
+        {showContent1 && (
+          <>
             <h2>Компонент Content1</h2>
-            <Content1/>
-            <hr />
+            <Content1 />
+          </>
+        )}
+      </div>
+
+      <hr />
+      
+      <div>
+        <button onClick={() => setShowContent2(!showContent2)}>
+          {showContent2 ? "Сховати Content2" : "Показати Content2"}
+        </button>
+        {showContent2 && (
+          <>
             <h2>Компонент Content2</h2>
-            <Content2/>
-            <hr />
+            <Content2 />
+          </>
+        )}
+      </div>
+
+      <hr />
+      
+      <div>
+        <button onClick={() => setShowFooter(!showFooter)}>
+          {showFooter ? "Сховати Footer" : "Показати Footer"}
+        </button>
+        {showFooter && (
+          <>
             <h2>Footer</h2>
             <Footer />
-        </div>
-    )
+          </>
+        )}
+      </div>
+    </div>
+  );
 }
